b884a06294d73d1c997b35457f7bc1928fab00c2
AlvaRobert <alvadawq@gmx.de>
2018-01-29 21:48:16 +0100

    template final

--- buildDocs.sh
+++ buildDocs.sh
@@ -2,12 +2,10 @@
 
 cp index.md index_PDF.md
 
-sed -i -e "s/add1.PNG)/add1.PNG){width=40%}/g" index_PDF.md
-sed -i -e "s/moveOp.PNG)/moveOp.PNG){width=70%}/g" index_PDF.md
-sed -i -e "s/DNoverloadable.PNG)/DNoverloadable.PNG){width=60%}/g" index_PDF.md
 sed -i -e "s/copyOp.PNG)/copyOp.PNG){height=60%}/g" index_PDF.md
+sed -i -e "s/DNoverloadable.PNG)/DNoverloadable.PNG){height=60%}/g" index_PDF.md
+sed -i -e "s/moveOp.PNG)/moveOp.PNG){height=60%}/g" index_PDF.md
 sed -i -e "s/syntax.PNG)/syntax.PNG){height=60%}/g" index_PDF.md
-
 
 pandoc assets/templates/header.md  index_PDF.md -o assets/documents/index.pdf --template assets/templates/eisvogel.tex --listings --pdf-engine=xelatex
 
